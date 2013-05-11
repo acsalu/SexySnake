a40f22c5106a79b371ec7f0f8079c0d100e161e4
Acsa Lu <acsa0210@gmail.com>
2013-05-11 20:51:57 +0800

    create SSMap class

---
+++ SexySnake/SSMap.h
@@ -0,0 +1,13 @@
+//
+//  SSMap.h
+//  SexySnake
+//
+//  Created by Acsa Lu on 5/11/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSMap : NSObject
+
+@end
---
+++ SexySnake/SSMap.m
@@ -0,0 +1,13 @@
+//
+//  SSMap.m
+//  SexySnake
+//
+//  Created by Acsa Lu on 5/11/13.
+//
+//
+
+#import "SSMap.h"
+
+@implementation SSMap
+
+@end
